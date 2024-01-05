f6066522a8bee977903d7e46dac07578624c23f9
JethroNatividad <natividad.jet@gmail.com>
2024-01-05 22:48:46 +0800

    write pseudocode

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,22 @@
 // Process: add 5.5% tax if state is Winsconsin or WI, if not, then leave it.
 // Outputs: (Winsconsin) Subtotal, Tax, Total | (Other states) Total.
 
-fn calculate_tax(order_amount: f64, tax_percentage: f64) -> f64 {
-    
+fn calculate_tax(order_amount: f64, tax_percentage: f64) -> (f64, f64) {
+    // calculate the tax
+    // round to 2 digits
+    // return (tax, total)
+
 }
 
 fn main() {
-    println!("Hello, world!");
+    // states = [{names: ["winsconsin", "wi"], tax_percentage: 5.5}]
+    // Prompt for order_amount with message "What is the order amount? "
+    // Prompt for state with message "What is the state? "
+    // if state is in states
+        // get the tax and total
+        // print "The subtotal is ${}."
+        // print "The tax is ${}."
+        // print "The total is ${}."
+    // else
+        // print "The total is $10.00."
 }
