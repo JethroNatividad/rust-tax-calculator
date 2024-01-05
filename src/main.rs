0c613916d376b7e6cbb8a5003cca06516cd76bf3
JethroNatividad <natividad.jet@gmail.com>
2024-01-05 22:51:24 +0800

    write round_decimal

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,19 @@
 // Process: add 5.5% tax if state is Winsconsin or WI, if not, then leave it.
 // Outputs: (Winsconsin) Subtotal, Tax, Total | (Other states) Total.
 
+fn round_decimal(number: f64, place: i64) -> f64 {
+    let multiplier = 10_f64.powi(place as i32);
+    (number * multiplier).round() / multiplier
+}
+
 fn calculate_tax(order_amount: f64, tax_percentage: f64) -> (f64, f64) {
     // calculate the tax
     // round to 2 digits
     // return (tax, total)
 
 }
+
+
 
 fn main() {
     // states = [{names: ["winsconsin", "wi"], tax_percentage: 5.5}]
