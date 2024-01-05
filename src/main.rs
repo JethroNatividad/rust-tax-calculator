0f967b7fbff61ab50a29022ae437dd8d509e5697
JethroNatividad <natividad.jet@gmail.com>
2024-01-05 22:40:25 +0800

    write problem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// write a simple program to compute tax and amount. Only add the tax if the state is Winsconsin or WI.
+
+// Inputs: Order amount, and state.
+// Process: add 5.5% tax if state is Winsconsin or WI, if not, then leave it.
+// Outputs: (Winsconsin) Subtotal, Tax, Total | (Other states) Total.
+
+fn calculate_tax(order_amount: f64, tax_percentage: f64) -> f64 {
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
